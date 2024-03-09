a1a78d96109cd57e2d2ec33468785716946ff9e4
EfthimiosVlahos <efthimios.vlahos@stonybrook.edu>
2024-03-09 16:27:23 -0500

    added code

--- Trees/EXERCISE-BST-Contains.py
+++ Trees/EXERCISE-BST-Contains.py
@@ -30,11 +30,16 @@ class BinarySearchTree:
                 temp = temp.right
 
     ## WRITE CONTAINS METHOD HERE ##
-    #                              #
-    #                              #
-    #                              #
-    #                              #
-    ################################
+    def contains(self, value):
+        temp = self.root
+        while (temp is not None):
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                return True
+        return False
         
 
 
